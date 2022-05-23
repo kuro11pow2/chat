2db3547512ef412583c1f5fd71b51329e73ba800
kuro11pow2 <kuro11pow2@gmail.com>
2022-05-23 23:52:02 +0900

    에코 서버 추가

---
+++ csharp/echo-server/echo-server/Log.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Threading;
+
+namespace echo_server
+{
+    public enum LogLevel
+    {
+        ERROR,
+        INFO,
+        DEBUG
+    }
+
+    public class Log
+    {
+        public static LogLevel PrintLevel = LogLevel.INFO;
+        private const int Width = 3;
+        private delegate void OutStream(string obj);
+        private static OutStream Out = Console.WriteLine;
+        private static string _line = "--------------------------------------------------------------------------------------------------------------------";
+
+        /// <summary>
+        /// 디버그 모드에서만 메소드 호출된다던데, 파라미터는 평가되는건지 성능 비교를 통해 파악해보기
+        /// 디버그 vs 릴리즈
+        /// 출력 없는 디버그 vs 출력 없는 릴리즈
+        /// </summary>
+        /// <param name="msg"></param>
+        /// <param name="level"></param>
+        /// <param name="context"></param>
+        //[Conditional("DEBUG")] NsLog를 여기에 매핑할 거라 함수 살아있어야 함. 그래서 주석처리
+        public static void Print(string msg, LogLevel level = LogLevel.DEBUG, string context = "")
+        {
+            if (level <= PrintLevel)
+            {
+                string printMsg;
+                printMsg = FormattedString(
+                    DateTime.Now.ToString("mm.ss.ffff"),
+                    ThreadPool.ThreadCount,
+                    ThreadPool.PendingWorkItemCount,
+                    Thread.CurrentThread.ManagedThreadId,
+                    context,
+                    LogLevelTag(level),
+                    $"{msg}\n{_line}");
+                Out(printMsg);
+            }
+        }
+        private static string LogLevelTag(LogLevel level)
+        {
+            switch (level)
+            {
+                case LogLevel.ERROR:
+                    return "ERROR";
+                case LogLevel.INFO:
+                    return "INFO";
+                case LogLevel.DEBUG:
+                    return "DEBUG";
+                default:
+                    return "WRONG LEVEL";
+            }
+        }
+
+        //[Conditional("DEBUG")]
+        public static void PrintLine()
+        {
+            Out(_line);
+        }
+
+        //[Conditional("DEBUG")]
+        public static void PrintHeader()
+        {
+            if (PrintLevel > LogLevel.ERROR)
+                Out(FormattedString(
+                    "mm.ss.ffff",
+                    "Avail",
+                    "Pend",
+                    "TID",
+                    "Context",
+                    "Type",
+                    "Message") + $"\n{_line}");
+        }
+
+        private static string FormattedString(object time, object tall, object tpend, object tid, object contect, object type, object msg)
+        {
+            return $"| {time,-9} | {tall,-5} | {tpend,-4} | {tid,-4} | {contect,-34} | {type,-9} | {msg,-1}";
+        }
+    }
+}
---
+++ csharp/echo-server/echo-server/Program.cs
@@ -0,0 +1,80 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System;
+using System.Threading.Tasks;
+using System.Text;
+using System.IO;
+
+
+namespace echo_server
+{
+    internal class Program
+    {
+        async static Task AysncEchoServer()
+        {
+            TcpListener listener = new TcpListener(IPAddress.Any, 7000);
+            listener.Start();
+            while (true)
+            {
+                // 비동기 Accept                
+                TcpClient tc = await listener.AcceptTcpClientAsync();
+
+                // 새 쓰레드에서 처리
+                _ = Task.Run(() =>
+                {
+                    AsyncTcpProcess(tc);
+                });
+            }
+        }
+
+        async static void AsyncTcpProcess(object o)
+        {
+            TcpClient tc = (TcpClient)o;
+
+            int MAX_SIZE = tc.ReceiveBufferSize;
+            NetworkStream stream = tc.GetStream();
+
+
+            // 비동기 수신            
+            var buff = new byte[MAX_SIZE];
+
+            while (true)
+            {
+                try
+                {
+                    int nbytes = await stream.ReadAsync(buff, 0, buff.Length);
+
+                    if (nbytes > 0)
+                    {
+                        string msg = Encoding.ASCII.GetString(buff, 0, nbytes);
+                        Log.Print($"{msg} at {DateTime.Now}");
+
+                        // 비동기 송신
+                        await stream.WriteAsync(buff, 0, nbytes);
+                    }
+                    else
+                    {
+                        Log.Print("0 byte 수신하여 종료");
+                        break;
+                    }
+                }
+                catch (IOException ex)
+                {
+                    Log.Print($"\n{ex}");
+                }
+            }
+
+            stream.Close();
+            tc.Close();
+        }
+
+        static void Main(string[] args)
+        {
+            Log.PrintLevel = LogLevel.DEBUG;
+
+            Log.PrintHeader();
+
+            AysncEchoServer().Wait();
+        }
+    }
+}
