984616d80aed19cfe9cde2f57dd6f1d9c78b01ab
kuro11pow2 <kuro11pow2@gmail.com>
2022-06-26 01:15:15 +0900

    벤치마크 코드 추가
    * PayloadProtocol Encode / Decode 벤치마크 코드 추가
    
    [BenchmarkDotNet](https://benchmarkdotnet.org/articles/overview.html)

---
+++ csharp/chat-module-0.3/Benchmarks/PayloadProtocolBenchmark.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Text;
+using System.Security.Cryptography;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace Benchmarks
+{
+    using Common;
+
+    static class StringGenerator
+    {
+        public static string GetKoreanString(int length)
+        {
+            Random random = new Random();
+            StringBuilder stringBuilder = new StringBuilder();
+            int rangeStart = 0xAC00, rangeEnd = 0xD7A3;
+
+            for (int i = 0; i < length; i++)
+            {
+                Rune rune = new Rune(random.Next(rangeStart, rangeEnd));
+                stringBuilder.Append(rune);
+            }
+
+            return stringBuilder.ToString();
+        }
+
+        public static string GetJapaneseString(int length)
+        {
+            Random random = new Random();
+            StringBuilder stringBuilder = new StringBuilder();
+            int rangeStart = 0x4E00, rangeEnd = 0x9fBF;
+
+            for (int i = 0; i < length; i++)
+            {
+                Rune rune = new Rune(random.Next(rangeStart, rangeEnd));
+                stringBuilder.Append(rune);
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
+
+    static class Utf8BytesGenerator
+    {
+
+        public static byte[] GetKorean(int length)
+        {
+            if (length % 3 != 0)
+                throw new InvalidDataException($"UTF8 한글은 글자당 3바이트이므로, 바이트는 3의 배수로 입력되어야 한다. current value : {length}");
+            return Encoding.UTF8.GetBytes(StringGenerator.GetKoreanString(length / 3));
+        }
+
+
+        public static byte[] GetJapanese(int length)
+        {
+            if (length % 3 != 0)
+                throw new InvalidDataException($"UTF8 한자는 글자당 3바이트이므로, 바이트는 3의 배수로 입력되어야 한다. current value : {length}");
+            return Encoding.UTF8.GetBytes(StringGenerator.GetJapaneseString(length / 3));
+        }
+    }
+
+
+    public class PayloadProtocolEncodeBenchmark
+    {
+        private const int STRING_LENGTH = 10000;
+
+        private readonly string utf8_data;
+
+        public PayloadProtocolEncodeBenchmark()
+        {
+            utf8_data = StringGenerator.GetKoreanString(STRING_LENGTH);
+        }
+
+        [Benchmark]
+        public byte[] Utf8Encode() => Utf8PayloadProtocol.Encode(utf8_data);
+    }
+
+    public class PayloadProtocolDecodeBenchmark
+    {
+        private const int BYTES_LENGTH = 30000;
+
+        private const int OFFSET = Utf8PayloadProtocol.SIZE_BYTES_LENGTH;
+        private const int UTF8_DATA_SIZE = OFFSET + BYTES_LENGTH;
+        private readonly byte[] utf8_data;
+
+        public PayloadProtocolDecodeBenchmark()
+        {
+            utf8_data = new byte[UTF8_DATA_SIZE];
+            Buffer.BlockCopy(Utf8BytesGenerator.GetKorean(BYTES_LENGTH), 0, utf8_data, OFFSET, BYTES_LENGTH);
+        }
+
+        [Benchmark]
+        public string Utf8Decode() => Utf8PayloadProtocol.Decode(utf8_data, UTF8_DATA_SIZE);
+    }
+}
---
+++ csharp/chat-module-0.3/Benchmarks/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Security.Cryptography;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace Benchmarks
+{
+    public class Md5VsSha256
+    {
+        private const int N = 10000;
+        private readonly byte[] data;
+
+        private readonly SHA256 sha256 = SHA256.Create();
+        private readonly MD5 md5 = MD5.Create();
+
+        public Md5VsSha256()
+        {
+            data = new byte[N];
+            new Random(42).NextBytes(data);
+        }
+
+        [Benchmark]
+        public byte[] Sha256() => sha256.ComputeHash(data);
+
+        [Benchmark]
+        public byte[] Md5() => md5.ComputeHash(data);
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+#if RELEASE      
+            BenchmarkDotNet.Reports.Summary sum;
+            sum = BenchmarkRunner.Run<PayloadProtocolEncodeBenchmark>();
+            //sum = BenchmarkRunner.Run<PayloadProtocolDecodeBenchmark>();
+#elif DEBUG
+            var encode = new Utf8PayloadProtocolEncodeBenchmark();
+            var encode_out = encode.Encode();
+            var decode = new Utf8PayloadProtocolDecodeBenchmark();
+            var decode_out = decode.Decode();
+#endif
+        }
+    }
+}
